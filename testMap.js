1ff91b0fd555ba16d96e7f2b412c660e20501856
小嶋洋輔 <yousuke@kojimayousukenoMacBook-Pro.local>
2016-11-23 02:18:48 +0900

    公共クラウドのデータ読み込み成功

--- testMap.js
+++ testMap.js
@@ -1,9 +1,38 @@
+var map;
+    
 function initMap(){
 
     var mapId ={
-	zoom: 15,
-	center: new google.maps.LatLng(35.6328964,139.8782003)
+	zoom: 5,
+	center: new google.maps.LatLng(35.890358,136.344221)
     };
 
-    var map = new google.maps.Map(document.getElementById("map"),mapId);
+    map = new google.maps.Map(document.getElementById("map"),mapId);
 }
+
+function setMaker(lat, lng, info, img){
+
+    var latlng = new google.maps.LatLng(lat,lng);
+    var marker = new google.maps.Marker({
+	position: latlng,
+	map: map
+    });
+
+    
+/*    var infoWindow = new google.maps.InfoWindow({
+	content: '<div class="marker"><a href=' + img + ' target=_blank><img width=100% src=' + img + '></a>'+info+'</div>'
+    });
+*/
+    var infoWindow = new google.maps.InfoWindow({
+	content: '<div class="marker">'+info+'</div>'
+    });
+    
+    google.maps.event.addListener(marker,'mouseover',function(){
+	infoWindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker,'mouseout',function(){
+	infoWindow.close();
+    });
+}
+ 
